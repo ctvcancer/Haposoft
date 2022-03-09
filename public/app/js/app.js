f36d15f03bf371c1339c4f8649d881f060bca0e7
caothevinh371999@gmail.com <caothevinh371999@gmail.com>
2022-03-09 10:37:10 +0700

    fix name img

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,7 +1,9 @@
-const navBarIcon = document.querySelector(".nav__bar__icon")
+const navBarIcon = document.querySelector(".nav-bar-icon")
             const menu = document.querySelector("#menu")
 
             navBarIcon.addEventListener("click", () => {
-                menu.classList.toggle("active__menu")
+                navBarIcon.classList.toggle('fa-xmark')
+                menu.classList.toggle("active-menu")
             })
             console.log(navBarIcon);
+            
